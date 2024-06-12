e40f01e41b3697c24f63686ac62803f359bcd500
Edward Rusli <erusli@sabarjaya.com>
2024-06-13 01:16:04 +0700

    added dialogue system
    todo next: add customer spawner

--- scripts/customer.gd
+++ scripts/customer.gd
@@ -1,42 +1,87 @@
 extends Node2D
 
-var button_child
-var label_child
-var orders = ["plate_cooked_seaweed", "plate_cooked_seaweed"]
-var orders_string
+@onready var button: TextureButton = $TextureButton
+@onready var test_label: Label = $Label
+@onready var dialogue_bubble: Node2D = $DialogueBubble
+@onready var dialogue_timer: Timer = $DialogueTimer
+
+signal on_order_received
+signal on_all_orders_finished
+
+var orders: Array[String] = ["plate_cooked_seaweed", "plate_cooked_seaweed"]
+#var orders: Array[String]
+var orders_string # plaintext of orders
+
+var current_line
+var dialogue_lines: Array[String] = [
+	"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+	"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+	"According to all known laws of aviation, there is no way that a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyways. Because bees don't care what humans think is impossible."
+]
+#var dialogue_lines: Array[String]
+
+var dialogue_breakpoints: Array[int] = [
+	#which lines does it want to get stuck on? (don't autoscroll after this line) (0 based)
+	1
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	button_child = get_child(0)
-	label_child = get_child(1)
+	button = get_child(0)
+	test_label = get_child(1)
 	
 	orders_string = ''
 	for i in orders:
 		orders_string = orders_string + i + '\n'
-	label_child.text = orders_string
+	test_label.text = orders_string
+	
+	await get_tree().create_timer(2.0).timeout
 
+	dialogue_bubble.current_line_finished_typing.connect(_on_dialogue_line_finished_printing)
+
+	dialogue_bubble.lines = dialogue_lines
+	dialogue_bubble.show()
+	current_line = 0
+	dialogue_bubble.print_dialogue(current_line)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if (Input.is_action_just_released("mouse_left")&&button_child.is_hovered()&&SceneManager.currentlyHeld != null):
+	if (Input.is_action_just_released("mouse_left")&&button.is_hovered()&&SceneManager.currentlyHeld != null):
 		#if something is dropped onto this pan
 
 		#check if the thing is in the order list
 		var dropped_thing_index = orders.find(SceneManager.currentlyHeld.itemName)
 		
-		if (dropped_thing_index != - 1):
+		if (dropped_thing_index != - 1): # if it is a valid item
 			#remove item from orders
 			orders.pop_at(dropped_thing_index)
 
 			#empty the origin plate
 			SceneManager.currentlyHeld.empty_plate()
 
+			on_order_received.emit()
+
 			#update orders
 			orders_string = ''
 			for i in orders:
 				orders_string = orders_string + i + '\n'
-			label_child.text = orders_string
-
+			test_label.text = orders_string
+			
+			next_line() # fix this asap, if food given before finished yapping, it could skip to next dialogue -> softlock
+			
 			if (orders.size() == 0):
-				self.queue_free()
+				on_all_orders_finished.emit()
+				queue_free()
 
 			print(orders)
+
+func _on_dialogue_line_finished_printing():
+	if (!dialogue_breakpoints.has(dialogue_bubble.curr_line)):
+		dialogue_timer.start()
+
+func _on_dialogue_timer_timeout():
+	next_line()
+
+func next_line():
+	current_line += 1
+	dialogue_bubble.print_dialogue(current_line)
---
+++ scripts/dialogue_bubble.gd
@@ -0,0 +1,46 @@
+extends Node2D
+
+@onready var label: Label = $ColorRect/MarginContainer/DialogueLabel
+@onready var timer: Timer = $Timer
+
+@export var lines: Array[String]
+var curr_line
+var curr_letter = -1
+
+signal current_line_finished_typing
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func print_dialogue(line: int):
+	#starts dialogue if just beginning, proceeds to next line if currently active
+	
+	curr_letter = 0
+	curr_line = line
+	if (curr_line >= lines.size()||curr_line == - 1):
+		#if out of lines
+		print("out of lines")
+		queue_free()
+	else:
+		label.text = ''
+		#begin printing letters
+		next_letter()
+
+func next_letter():
+	label.text += lines[curr_line][curr_letter]
+	curr_letter += 1
+	if (curr_letter >= lines[curr_line].length()):
+		#if current line is finished printing
+		print('ended')
+		current_line_finished_typing.emit()
+		#await get_tree().create_timer(2.0).timeout
+		#start_or_next_dialogue()
+	else:
+		timer.start(0.005)
+
+func _on_timer_timeout():
+	next_letter()
